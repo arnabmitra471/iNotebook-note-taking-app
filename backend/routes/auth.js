33dedaa9e2b79e77427da09b29c41f2ef2dab4e0
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-01 18:58:26 +0530

    Added validation for the fields in UserSchema with express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users")
+const {query} = require("express-validator")
 
 // Create a user using POST "/api/auth"
-router.get("/",(req,res)=>{
+router.post("/",[
+    query("name").isLength({min: 3}),
+    query("email").isEmail(),
+    query("password").isLength({min:5})
+],(req,res)=>{
     console.log(req.body);
     const user = User(req.body)
     user.save();
