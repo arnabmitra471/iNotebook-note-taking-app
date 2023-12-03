9e349c90d73f0b88d3a995f275785f909d10bccd
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-03 11:18:02 +0530

    Hashing passwords after generating their salt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,38 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users")
-const {body,validationResult} = require("express-validator")
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 // Create a user using POST "/api/auth"
-router.post("/createuser",[
+router.post("/createuser", [
     // Applying validation based on the fields in our schema
-    body("name","Please enter a valid name").isLength({min: 3}),
-    body("email","Please enter a valid email").isEmail(),
-    body("password","Please enter a valid password").isLength({min:5})
-],async(req,res)=>{
+    body("name", "Please enter a valid name").isLength({ min: 3 }),
+    body("email", "Please enter a valid email").isEmail(),
+    body("password", "Please enter a valid password").isLength({ min: 5 })
+], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty())
-    {
-        return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
     }
-    // check whether the user with this email exists already
-    try
-    {
-    let user = await User.findOne({email: req.body.email})
-    if(user)
-    {
-      return res.status(400).json({error:"Sorry a user with this email already exists"})  
+    try {
+        // check whether the user with this email exists already
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass
+        })
+        console.log(user)
+        res.json({ success: "User Added" })
     }
-    user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-    console.log(user)
-    res.json({success: "User Added"})
-    }
-    catch(err)
-    {
+    catch (err) {
         console.error(err.message)
         res.status(500).send("Some error occured")
     }
