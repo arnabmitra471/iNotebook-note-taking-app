98b9a65469a602a8ba93d902fc88b28ddeca316b
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-03 12:22:12 +0530

    Generating the jsonwebtoken for authentication of a user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const User = require("../models/Users")
 const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
-
+const jwt = require("jsonwebtoken");
 // Create a user using POST "/api/auth"
+
+const JWT_SECRET = "Fd5$gkloPjAsRtsxZa"
 router.post("/createuser", [
     // Applying validation based on the fields in our schema
     body("name", "Please enter a valid name").isLength({ min: 3 }),
@@ -27,9 +29,15 @@ router.post("/createuser", [
             name: req.body.name,
             email: req.body.email,
             password: secPass
-        })
-        console.log(user)
-        res.json({ success: "User Added" })
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const auth_token = jwt.sign(data,JWT_SECRET)
+        console.log(auth_token)
+        res.json({auth_token})
     }
     catch (err) {
         console.error(err.message)
