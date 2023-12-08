c757987b8e1a0c6fcc35f70311b1a10092f247ed
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-08 20:59:19 +0530

    If the note does not belong to the particular authenticated user then sending a 401 Unauthorized status code

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,13 +7,11 @@ const fetchUser = require("../middleware/fetchUser")
 // Route 1: fetch all notes of a user using GET /api/notes/fetchallnotes Login Required
 
 router.get("/fetchallnotes", fetchUser, async (req, res) => {
-    try
-    {
+    try {
         const notes = await Note.find({ user: req.user.id })
         res.json(notes);
     }
-    catch(err)
-    {
+    catch (err) {
         console.error(err.message);
         res.status(500).send("Internal Server error");
     }
@@ -34,43 +32,47 @@ router.post("/addnote", fetchUser, [
             }
             const { title, description, tag } = req.body;
             const note = new Note({
-                title, description, tag,user: req.user.id
+                title, description, tag, user: req.user.id
             })
             const savedNote = await note.save();
 
             res.json(savedNote);
         }
-        catch(err)
-        {
+        catch (err) {
             console.error(err.message);
             res.status(500).send("Internal Server error");
         }
     })
 
-router.put("/updatenote/:id",fetchUser,async (req,res)=>{
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
 
-    let {title,description,tag} = req.body
+    let { title, description, tag } = req.body
     //Create a newNote object
-    let newNote = {};
-    if(title)
-    {
-        newNote.title = title;
+    try {
+        let newNote = {};
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+        // find the note by to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
     }
-    if(description)
-    {
-        newNote.description = description;
+    catch(err) {
+        console.error(err.message);
+        return res.status(500).send("Internal Server Error")
     }
-    if(tag)
-    {
-        newNote.tag = tag;
-    }
-    // find the note by to be updated and update it
-    let note = await Note.findById(req.params.id);
-    if(!note)
-    {
-        return res.status(404).send("Not Found");
-    }
-    note = await Note.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true});
-    res.json({note});
 })
 module.exports = router
