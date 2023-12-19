d4ff6e6e8cb8887ce4a20c9119be6ec901632607
Arnab Mitra <arnabmitra471@gmail.com>
2023-12-19 20:45:44 +0530

    Added an Alert component but it will be worked on later

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Navbar from "./components/Navbar"
 import About from "./components/About"
 import Home from "./components/Home"
 import NoteState from "./context/notes/NoteState"
+import Alert from "./components/Alert"
 function App() {
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="This is an amazing alert"/>
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />}></Route>
